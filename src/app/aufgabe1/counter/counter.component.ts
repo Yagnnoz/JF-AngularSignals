6d22a04f1b0487064dec7f509d78d38663e2964c
Yagnnoz <jensk37@gmail.com>
2025-11-24 22:05:27 +0100

    aufgabe 1

---
+++ src/app/aufgabe1/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+
+  count = signal<number>(0);
+  countMultiplied = computed(() => this.count() * 10);
+
+  addOne(): void {
+    this.count.update(num => num + 1);
+  }
+
+  subtractOne(): void {
+    this.count.update(num => num - 1);
+  }
+
+  logCountEffect = effect(() => {
+    console.log(this.count());
+  });
+}
